38fbf5f32f627e01561a42baa69f56a68c0ce108
shinemost <hjfu2019@gmail.com>
2024-05-17 08:12:14 +0000

    使用RestClient客户端调用

--- main.go
+++ main.go
@@ -22,11 +22,11 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
-	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	//
@@ -54,35 +54,37 @@ func main() {
 		panic(err.Error())
 	}
 
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
+	config.APIPath = "api"
+	config.GroupVersion = &corev1.SchemeGroupVersion
+
+	// 配置数据的编解码器
+	config.NegotiatedSerializer = scheme.Codecs
+
+	// create the restclient
+	restClient, err := rest.RESTClientFor(config)
 	if err != nil {
 		panic(err.Error())
 	}
-	for {
-		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
 
-		// Examples for error handling:
-		// - Use helper functions like e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		namespace := "olm"
-		pod := "olm-operator-79978456c6-dztsr"
-		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
-				pod, namespace, statusError.ErrStatus.Message)
-		} else if err != nil {
-			panic(err.Error())
-		} else {
-			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
-		}
+	result := &corev1.PodList{}
 
-		time.Sleep(10 * time.Second)
+	err = restClient.Get().
+		// Namespace("olm").
+		Resource("pods").
+		VersionedParams(&metav1.ListOptions{Limit: 100}, scheme.ParameterCodec).
+		Do(context.TODO()).
+		Into(result)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, d := range result.Items {
+		fmt.Printf(
+			"NAMESPACE:%v \t NAME: %v \t STATUS: %v\n",
+			d.Namespace,
+			d.Name,
+			d.Status.Phase,
+		)
 	}
 }
