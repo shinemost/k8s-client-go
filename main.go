c5a63277b7823b03e30965ae1c5c383c4d441aac
shinemost <hjfu2019@gmail.com>
2024-05-22 03:40:00 +0000

    实现一个简单的controller

--- main.go
+++ main.go
@@ -19,13 +19,18 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/discovery"
 	"path/filepath"
 
+	"github.com/shinemost/k8s-client-go/controller"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/klog/v2"
+
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/util/workqueue"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -51,27 +56,48 @@ func main() {
 		panic(err.Error())
 	}
 
-	// 通过 config 实例化 DiscoveryClient 对象
-	discoveryClient, err := discovery.NewDiscoveryClientForConfig(config)
+	// 创建 k8s Client
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err)
+		klog.Fatal(err)
 	}
 
-	// 返回 Kubernetes APIServer 所支持的资源组、资源版本和资源信息
-	_, APIResourceList, err := discoveryClient.ServerGroupsAndResources()
-	if err != nil {
-		panic(err)
-	}
+	// 从指定的客户端、资源、命名空间和字段选择器创建⼀个新的 List-Watch
+	podListWatcher := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(),
+		"pods", v1.NamespaceDefault, fields.Everything())
 
-	// 输出所有资源信息
-	for _, list := range APIResourceList {
-		gv, err := schema.ParseGroupVersion(list.GroupVersion)
-		if err != nil {
-			panic(err)
-		}
-		for _, resource := range list.APIResources {
-			fmt.Printf("NAME: %v, GROUP: %v, VERSION: %v \n", resource.
-				Name, gv.Group, gv.Version)
-		}
-	}
+	// 构造⼀个具有速率限制排队功能的新的 Workqueue
+	queue := workqueue.NewRateLimitingQueue(workqueue.DefaultControllerRateLimiter())
+
+	// 创建 Indexer 和 Informer
+	indexer, informer := cache.NewIndexerInformer(podListWatcher, &v1.Pod{},
+		0, cache.ResourceEventHandlerFuncs{
+			//当有Pod创建时，根据Delta Queue弹出的Object⽣成对应的Key，并加⼊Workqueue中。此处可以根据 Object 的⼀些属性进⾏过滤
+			AddFunc: func(obj interface{}) {
+				key, err := cache.MetaNamespaceKeyFunc(obj)
+				if err == nil {
+					queue.Add(key)
+				}
+			},
+			//Pod 删除操作
+			DeleteFunc: func(obj interface{}) {
+				// 在⽣成 Key 之前检查对象。因为资源删除后有可能会进⾏重建等操作，如果监听时错过
+				//了删除信息，会导致该条记录是陈旧的
+				key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+
+				if err == nil {
+					queue.Add(key)
+				}
+			},
+		}, cache.Indexers{})
+
+	// 创建新的 Controller
+	controller := controller.NewController(queue, indexer, informer)
+
+	stop := make(chan struct{})
+	defer close(stop)
+	// 启动 Controller
+	go controller.Run(1, stop)
+	select {}
+
 }
